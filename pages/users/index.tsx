504809d766b4aeeef93bc91e79b0838ee9f3d242
David Vargas <dvargas92495@gmail.com>
2020-09-04 20:04:46 -0400

    Fixing master action

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -11,12 +11,12 @@ type Props = {
 }
 
 const WithStaticProps = ({ items }: Props) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
-    <h1>Users List</h1>
+  <Layout title="Issues List | Floss">
+    <h1>Issues List</h1>
     <p>
       Example fetching data from inside <code>getStaticProps()</code>.
     </p>
-    <p>You are currently on: /users</p>
+    <p>You are currently on: /issues</p>
     <List items={items} />
     <p>
       <Link href="/">
@@ -27,9 +27,6 @@ const WithStaticProps = ({ items }: Props) => (
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
   const items: User[] = sampleUserData
   return { props: { items } }
 }
