43d14d322ad9a645145f21a3a2ed40c1bd1fdb2c
David Vargas <dvargas92495@gmail.com>
2020-09-11 17:56:57 -0400

    Fix lambda deploy

--- lambdas/github-issues_get.ts
+++ lambdas/github-issues_get.ts
@@ -5,20 +5,22 @@ const headers = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
 };
 
-export const handler = () => axios(`https://api.github.com/issues?filter=subscribed&state=open`, {
+export const handler = () =>
+  axios(`https://api.github.com/issues?filter=subscribed&state=open`, {
     headers: {
       Accept: "application/vnd.github.inertia-preview+json",
       Authorization: `Basic ${Buffer.from(
         `dvargas92495:${process.env.PERSONAL_ACCESS_TOKEN}`
       ).toString("base64")}`,
     },
-  }).then((r) => ({
-    statusCode: 200,
-    body: JSON.stringify(r.data),
-    headers,
-  }))
-  .catch((e) => ({
-    statusCode: 500,
-    body: e.message,
-    headers,
-  }));
+  })
+    .then((r) => ({
+      statusCode: 200,
+      body: JSON.stringify(r.data),
+      headers,
+    }))
+    .catch((e) => ({
+      statusCode: 500,
+      body: e.message,
+      headers,
+    }));
