596471b181c7564901a74e0b5b7d36260df2fba7
David Vargas <dvargas92495@gmail.com>
2020-09-11 21:40:34 -0400

    Closes #4

--- pages/issues/index.tsx
+++ pages/issues/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/Layout";
 import List from "../../components/List";
 import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import { GetStaticProps } from "next";
@@ -10,13 +11,15 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import TextField from "@material-ui/core/TextField";
 import DialogActions from "@material-ui/core/DialogActions";
+import addMonths from "date-fns/addMonths";
+import format from "date-fns/format";
+
+const API_URL = `https://${process.env.NEXT_PUBLIC_REST_API_ID}.execute-api.us-east-1.amazonaws.com/production`;
 
 const IssueList = () => {
   const [items, setItems] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://${process.env.NEXT_PUBLIC_REST_API_ID}.execute-api.us-east-1.amazonaws.com/production/github-issues`
-    )
+    fetch(`${API_URL}/github-issues`)
       .then((res) => res.json())
       .then((res) => {
         setItems(
@@ -31,14 +34,82 @@ const IssueList = () => {
   }, [setItems]);
   return <List items={items} />;
 };
-const WithStaticProps = () => {
+
+const CreateGithubIssueForm = ({
+  handleClose,
+}: {
+  handleClose: () => void;
+}) => {
+  const [link, setLink] = useState("");
+  const [reward, setReward] = useState(100);
+  const [dueDate, setDueDate] = useState(addMonths(new Date(), 1));
+  const saveIssue = useCallback(
+    () =>
+      axios
+        .post(`${API_URL}/contract`, {
+          link,
+          reward,
+          dueDate,
+        })
+        .then((r) => console.log(r.data))
+        .then(handleClose),
+    [handleClose, link, reward, dueDate]
+  );
+
+  return (
+    <>
+      <DialogContent>
+        <DialogContentText>
+          To create a Github Issue Contract, please fill out the form below.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Github Issue Link"
+          fullWidth
+          required
+          variant="filled"
+          placeholder={"https://github.com/{owner}/{repo}/issues/{number}"}
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
+        <TextField
+          type={"number"}
+          required
+          variant="filled"
+          label={"Reward"}
+          placeholder={"100"}
+          value={reward}
+          onChange={(e) => setReward(parseInt(e.target.value))}
+        />
+        <TextField
+          type={"date"}
+          required
+          variant="filled"
+          label={"Due"}
+          value={format(dueDate, "yyyy-MM-dd")}
+          onChange={(e) => setDueDate(new Date(e.target.value))}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary">
+          Cancel
+        </Button>
+        <Button onClick={saveIssue} color="primary">
+          Create
+        </Button>
+      </DialogActions>
+    </>
+  );
+};
+
+const CreateGithubIssueDialog = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = useCallback(() => setOpen(true), [setOpen]);
   const handleClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
-    <Layout title="Issues List | Floss">
-      <Typography variant="h1">Issues List</Typography>
-      <IssueList />
+    <>
       <Button color="secondary" variant="contained" onClick={handleOpen}>
         ADD ISSUE
       </Button>
@@ -47,39 +118,22 @@ const WithStaticProps = () => {
         onClose={handleClose}
         aria-labelledby="issue-form-title"
       >
-        <DialogTitle id="issue-form-title">Create Github Issue Contract</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To create a Github Issue Contract, please fill out the form below.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Github Issue Link"
-            fullWidth
-            required
-            variant="filled"
-          />
-          <TextField
-            type={"number"}
-            required
-            variant="filled"
-            label={"Reward"}
-          />
-          <TextField type={"date"} required variant="filled" label={"Due"} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Create
-          </Button>
-        </DialogActions>
+        <DialogTitle id="issue-form-title">
+          Create Github Issue Contract
+        </DialogTitle>
+        <CreateGithubIssueForm handleClose={handleClose} />
       </Dialog>
-    </Layout>
+    </>
   );
 };
+
+const WithStaticProps = () => (
+  <Layout title="Issues List | Floss">
+    <Typography variant="h1">Issues List</Typography>
+    <IssueList />
+    <CreateGithubIssueDialog />
+  </Layout>
+);
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
