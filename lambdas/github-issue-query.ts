f9a1025e3bfc60740511d39f957daac73548854f
David Vargas <dvargas92495@gmail.com>
2020-09-23 13:04:09 -0400

    Automatic job completing projects

--- lambdas/github-issue-query.ts
+++ lambdas/github-issue-query.ts
@@ -1,17 +1,28 @@
 import axios from "axios";
-import { getActiveContracts } from "../utils/lambda";
+import { dynamo, getActiveContracts } from "../utils/lambda";
 
-export const handler = () => getActiveContracts().then(r => {
-  const uuidsByLink = Object.fromEntries(r.map(i => [i.link, i.uuid]))
-  const reqs = r.map(i => i.link ? axios(i.link.replace("github.com", "api.github.com/repos")) : Promise.resolve({} as any));
-  return Promise.all(reqs).then(issues => {
-    const ghIssues = issues.map(i => ({link: i.data.html_url, uuid: uuidsByLink[i.data.html_url],lifecycle: i.data.state}));
-    const completions = ghIssues.filter(i => i.lifecycle === 'closed');
-    const actives = ghIssues.filter(i => i.lifecycle === 'open');
-    console.log("Contracts to Complete:");
-    console.log(completions);
-    console.log("Contracts that are still active:");
-    console.log(actives);
-    console.log("Done!");
+export const handler = () =>
+  getActiveContracts().then((r) => {
+    const contractsByLink = Object.fromEntries(r.map((i) => [i.link, i]));
+    const reqs = r.map((i) =>
+      i.link
+        ? axios(i.link.replace("github.com", "api.github.com/repos"))
+        : Promise.resolve({} as any)
+    );
+    return Promise.all(reqs)
+      .then((issues) => {
+        const ghIssues = issues.map((i) => ({
+          link: { S: i.data.html_url },
+          uuid: { S: contractsByLink[i.data.html_url].uuid },
+          lifecycle: { S: i.data.state },
+          reward: { N: contractsByLink[i.data.html_url].reward },
+          dueDate: { S: contractsByLink[i.data.html_url].dueDate },
+        }));
+        const completions = ghIssues.filter((i) => i.lifecycle.S === "closed");
+        const completionPromises = completions.map((Item) =>
+          dynamo.putItem({ Item, TableName: "FlossContracts" }).promise()
+        );
+        return Promise.all(completionPromises);
+      })
+      .then((r) => console.log(`Completed ${r.length} Contracts!`));
   });
-})
