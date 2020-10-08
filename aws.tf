d259cf7b504555303c9c94cd83541236efddcb54
David Vargas <dvargas92495@gmail.com>
2020-10-08 10:52:47 -0400

    Upgrade Serveless Module

--- aws.tf
+++ aws.tf
@@ -49,7 +49,7 @@ module "aws-static-site" {
 
 module "aws-serverless-backend" {
     source  = "dvargas92495/serverless-backend/aws"
-    version = "1.2.1"
+    version = "1.3.0"
 
     api_name = "floss"
     paths = [
