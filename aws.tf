5e16462f3c342f9c421372fcbbd4e26c7ba07a60
David Vargas <dvargas92495@gmail.com>
2020-10-11 14:17:15 -0400

    Bump up serverless

--- aws.tf
+++ aws.tf
@@ -49,7 +49,7 @@ module "aws-static-site" {
 
 module "aws-serverless-backend" {
     source  = "dvargas92495/serverless-backend/aws"
-    version = "1.3.0"
+    version = "1.3.1"
 
     api_name = "floss"
     paths = [
