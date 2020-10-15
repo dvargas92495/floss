9734459841570b2f783ff5e65baf8d619f79e792
David Vargas <dvargas92495@gmail.com>
2020-10-15 19:39:10 -0400

    Landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
 import Layout from "../components/Layout";
-import Typography from "@material-ui/core/Typography";
 import React from "react";
 import FlossLogo from "../components/FlossLogo";
-import Grid from "@material-ui/core/Grid";
+import { Landing } from "@dvargas92495/ui";
 
 const IndexPage = () => (
   <Layout title="Floss | FreeLance Open Source Software">
-    <Grid container direction="column" alignItems="center">
-      <Typography variant="h1">
-        <FlossLogo size={20} />
-      </Typography>
-      <Typography variant="subtitle1">
-        <i>FreeLance Open Source Software</i>
-      </Typography>
-    </Grid>
+    <Landing Logo={FlossLogo} subtitle={"FreeLance Open Source Software"} />
   </Layout>
 );
 
