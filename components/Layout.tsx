5468eaf2b00e5295f19523f4396ae05877ac2043
David Vargas <dvargas92495@gmail.com>
2020-10-23 17:03:59 -0400

    Posting to twitter

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -74,7 +74,15 @@ const Layout: FunctionComponent<Props> = ({ children, title = "Floss" }) => {
               onClick={() =>
                 axios
                   .post(`${API_URL}/twitter-login`)
-                  .then((r) => console.log(r.data))
+                  .then((r) =>
+                    axios.post(
+                      "https://api.twitter.com/oauth/request_token",
+                      {
+                        oauth_callback: `${API_URL}/auth?twitter=true`,
+                      },
+                      { headers: r.data }
+                    )
+                  )
                   .catch((e) => console.error(e))
               }
             >
