730262f72233abfe3145bf68545b8c467be23660
David Vargas <dvargas92495@gmail.com>
2020-10-23 20:20:34 -0400

    tokening

--- lambdas/twitter-auth_post.ts
+++ lambdas/twitter-auth_post.ts
@@ -20,22 +20,22 @@ export const handler = async (event: APIGatewayEvent) => {
       const parsedData = Object.fromEntries(
         r.data.split("&").map((s: string) => s.split("="))
       );
-      const { oauth_token } = parsedData;
+      const { oauth_token, oauth_token_secret } = parsedData;
       const credentialHeaders = twitterOAuth.toHeader(
-        twitterOAuth.authorize({
-          url: `https://api.twitter.com/1.1/account/verify_credentials?oauth_token=${oauth_token}`,
-          method: "GET",
-        })
+        twitterOAuth.authorize(
+          {
+            url: `https://api.twitter.com/1.1/account/verify_credentials`,
+            method: "GET",
+          },
+          { key: oauth_token, secret: oauth_token_secret }
+        )
       );
       console.log(parsedData);
       console.log(credentialHeaders);
       return axios
-        .get(
-          `https://api.twitter.com/1.1/account/verify_credentials?oauth_token=${oauth_token}`,
-          {
-            headers: credentialHeaders,
-          }
-        )
+        .get(`https://api.twitter.com/1.1/account/verify_credentials`, {
+          headers: credentialHeaders,
+        })
         .then((c) => ({
           statusCode: 200,
           body: JSON.stringify(c.data),
