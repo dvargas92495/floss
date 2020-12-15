d1594568a630edcac22034f1276846cb01dbd356
David Vargas <dvargas92495@gmail.com>
2020-12-15 14:05:19 -0800

    Deploy stripe auth0 endpoint

---
+++ lambdas/stripe-user_put.ts
@@ -0,0 +1,29 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { headers, stripe } from "../utils/lambda";
+import { ManagementClient } from "auth0";
+
+const client = new ManagementClient({
+  clientId: process.env.AUTH0_CLIENT_ID || "",
+  clientSecret: process.env.AUTH0_CLIENT_SECRET || "",
+  domain: "vargas-arts.us.auth0.com",
+});
+
+export const handler = async (event: APIGatewayEvent) => {
+  const { id, email } = JSON.parse(event.body || "{}");
+  const existingCustomers = await stripe.customers.list({
+    email,
+  });
+  const customer = existingCustomers.data.length
+    ? existingCustomers.data[0]
+    : await stripe.customers.create({
+        email,
+      });
+  await client.updateAppMetadata({ id }, { stripe: customer.id });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      success: true,
+    }),
+    headers,
+  };
+};
