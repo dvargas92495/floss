f583ff619c6f995a015e1d69cddedb34ecb5e1e4
David Vargas <dvargas92495@gmail.com>
2020-12-15 12:44:30 -0800

    Create Stripe User PUT endpoint

--- aws.tf
+++ aws.tf
@@ -76,6 +76,7 @@ module "aws-serverless-backend" {
         "stripe-setup-intent/post",
         "stripe-payment-methods/get",
         "stripe-payment-succeeded/post",
+        "stripe-user/put",
         "twitter-auth/post",
         "twitter-auto/post",
         "twitter-login/post"
