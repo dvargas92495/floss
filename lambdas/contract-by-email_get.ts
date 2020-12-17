75da5411929d42458be0745e129a743bdd239815
David Vargas <dvargas92495@gmail.com>
2020-12-16 22:02:37 -0800

    Log error

--- lambdas/contract-by-email_get.ts
+++ lambdas/contract-by-email_get.ts
@@ -8,49 +8,53 @@ import {
 } from "../utils/lambda";
 
 export const handler = async (event: APIGatewayEvent) =>
-  getAuth0UserFromEvent(event).then((user) =>
-    dynamo
-      .query({
-        TableName: "FlossContracts",
-        KeyConditionExpression: "createdBy = :c and lifecycle = :s",
-        IndexName: "createdBy-lifecycle-index",
-        ExpressionAttributeValues: {
-          ":c": {
-            S: user.email,
+  getAuth0UserFromEvent(event)
+    .then((user) =>
+      dynamo
+        .query({
+          TableName: "FlossContracts",
+          KeyConditionExpression: "createdBy = :c and lifecycle = :s",
+          IndexName: "createdBy-lifecycle-index",
+          ExpressionAttributeValues: {
+            ":c": {
+              S: user.email,
+            },
+            ":s": {
+              S: "active",
+            },
           },
-          ":s": {
-            S: "active",
-          },
-        },
-      })
-      .promise()
-      .then((r) =>
-        r.Count && r.Items
-          ? Promise.all(
-              r.Items.map((i) =>
-                getAxiosByGithubLink(i.link.S).then((g) => ({
-                  name: g.title,
-                  label: g.labels
-                    ? g.labels.map(({ name }) => name).join(",")
-                    : "",
-                  uuid: i.uuid.S,
-                  link: i.link.S,
-                  ...parsePriority(i),
-                }))
-              )
+        })
+        .promise()
+    )
+    .then((r) =>
+      r.Count && r.Items
+        ? Promise.all(
+            r.Items.map((i) =>
+              getAxiosByGithubLink(i.link.S).then((g) => ({
+                name: g.title,
+                label: g.labels
+                  ? g.labels.map(({ name }) => name).join(",")
+                  : "",
+                uuid: i.uuid.S,
+                link: i.link.S,
+                ...parsePriority(i),
+              }))
             )
-          : Promise.resolve([])
-      )
-      .then((contracts) => ({
-        statusCode: 200,
-        body: JSON.stringify({
-          contracts,
-        }),
-        headers,
-      }))
-      .catch((e) => ({
+          )
+        : Promise.resolve([])
+    )
+    .then((contracts) => ({
+      statusCode: 200,
+      body: JSON.stringify({
+        contracts,
+      }),
+      headers,
+    }))
+    .catch((e) => {
+      console.log(e);
+      return {
         statusCode: 500,
         body: e.message,
         headers,
-      }))
-  );
+      };
+    });
