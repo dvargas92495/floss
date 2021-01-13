48c4ed694f4c8a54d79dd15b6341f62fdec4a4ab
David Vargas <dvargas92495@gmail.com>
2021-01-13 13:34:50 -0500

    Restrict user metadata endpoint

--- lambdas/auth-user-metadata_get.ts
+++ lambdas/auth-user-metadata_get.ts
@@ -3,12 +3,17 @@ import { headers, auth0UserClient, auth0Client } from "../utils/lambda";
 
 export const handler = async (event: APIGatewayEvent) => {
   const eventHeaders = event.headers;
+  const query = event.queryStringParameters?.key;
   return auth0UserClient
     .getProfile(eventHeaders.Authorization.substring("Bearer ".length))
     .then((user) => auth0Client.getUser({ id: user.sub }))
     .then((user) => ({
       statusCode: 200,
-      body: JSON.stringify(user.user_metadata),
+      body: user.user_metadata
+        ? JSON.stringify(
+            query ? { [query]: user.user_metadata[query] } : user.user_metadata
+          )
+        : JSON.stringify({}),
       headers,
     }))
     .catch((e) => ({
