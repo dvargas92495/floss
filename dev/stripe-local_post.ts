ae64a663acaeb785fa6d8c76cc556ea6bffb8d77
David Vargas <dvargas92495@gmail.com>
2021-02-24 13:30:30 -0600

    Local stripe fixes

--- dev/stripe-local_post.ts
+++ dev/stripe-local_post.ts
@@ -8,24 +8,35 @@ const MAPPING = {
   "payment_method.attached": "stripe-payment-attached",
   "setup_intent.succeeded": "stripe-setup-succeeded",
   "invoice.paid": "stripe-invoice-paid",
-} as {[type: string]: string};
+} as { [type: string]: string };
 
 export const handler = async (event: APIGatewayEvent) => {
-  const stripeEvent: Stripe.Event = JSON.parse(
-    event.body || "{}"
-  );
+  const stripeEvent: Stripe.Event = JSON.parse(event.body || "{}");
   if (MAPPING[stripeEvent.type]) {
     return axios
-      .post(`http://localhost:3001/dev/${MAPPING[stripeEvent.type]}`, stripeEvent)
+      .post(
+        `http://localhost:3001/dev/${MAPPING[stripeEvent.type]}`,
+        stripeEvent
+      )
       .then((r) => ({
         statusCode: r.status,
         body: JSON.stringify(r.data),
         headers: r.headers,
-      }));
+      }))
+      .catch((e) => {
+        const body = e.response?.data || e.message;
+        console.error(body);
+        return {
+          statusCode: 500,
+          body,
+          headers,
+        };
+      });
   }
+  console.log("Ignoring Stripe event", stripeEvent.type);
   return {
     statusCode: 204,
-    body: JSON.stringify({}),
+    body: JSON.stringify({ event: stripeEvent.type }),
     headers,
   };
 };
